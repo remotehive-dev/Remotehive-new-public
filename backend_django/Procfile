ea8f82dc0554b7a87d0409d434e2bc84a1b97e9e
remotehive-dev <remotehive.official@gmail.com>
2025-12-30 10:54:09 +0530

    build(django): update deployment config for static files
    Add whitenoise middleware and static files configuration for production
    Set CSRF trusted origins for railway deployment
    Update Procfile to collect static files before starting gunicorn
    Add .gitignore for admin frontend

--- backend_django/Procfile
+++ backend_django/Procfile
@@ -1,1 +1,1 @@
-web: gunicorn remotehive_admin.wsgi:application --bind 0.0.0.0:$PORT
+web: python manage.py collectstatic --noinput && gunicorn remotehive_admin.wsgi:application --bind 0.0.0.0:$PORT
--- backend_django/remotehive_admin/settings.py
+++ backend_django/remotehive_admin/settings.py
@@ -31,6 +31,10 @@ DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 
 ALLOWED_HOSTS = ["*"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://remotehive-django-production.up.railway.app",
+]
+
 
 # Application definition
 
@@ -55,6 +59,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -177,6 +182,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 STATICFILES_DIRS = [
     BASE_DIR / "backend_django" / "static",
 ]
